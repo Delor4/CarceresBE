9b1d435128da9efcdd31aa969408e22c2830596a
Delor Rotstrump <delor@delor.pl>
2020-10-25 21:59:37 +0100

    using offset() and limit() in pagination query

--- classes/views.py
+++ classes/views.py
@@ -20,31 +20,31 @@ def list_view(model_class, resource_fields, resource_url):
 
 def get_paginated_list(model_class, resource_fields, resource_url, start, limit):
     # check if page exists
-    results = session.query(model_class).all()
-    count = len(results)
+    count = session.query(model_class).count()
+    # count = session.query(func.count(model_class.id)).scalar()
+
     # make response
-    obj = {'start': start, 'limit': limit, 'count': count}
+    obj = {'start': start, 'limit': limit, 'count': count, 'previous': '', 'next': ''}
     # check bounds
     if count == 0:
         obj['results'] = []
         return obj
     if start < 1 or count < start:
         abort(404, message=f"Pagination start outside allowed values. Expected: 1 - {count}. Provided: {start}")
+
     # make URLs
     # make previous url
-    if start == 1:
-        obj['previous'] = ''
-    else:
+    if start != 1:
         start_copy = max(1, start - limit)
         limit_copy = start - 1
         obj['previous'] = resource_url + '?start=%d&limit=%d' % (start_copy, limit_copy)
     # make next url
-    if start + limit > count:
-        obj['next'] = ''
-    else:
+    if start + limit <= count:
         start_copy = start + limit
         obj['next'] = resource_url + '?start=%d&limit=%d' % (start_copy, limit)
+
     # finally extract result according to bounds
-    obj['results'] = marshal(results[(start - 1):(start - 1 + limit)], resource_fields)
+    results = session.query(model_class).limit(limit).offset(start - 1).all()
+    obj['results'] = marshal(results, resource_fields)
 
     return obj
