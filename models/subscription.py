d7d44d55cf394015e16db14a5536699783127c28
Delor Rotstrump <delor@delor.pl>
2020-11-25 11:12:23 +0100

    added notification_sended field to Subscription model

--- models/subscription.py
+++ models/subscription.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import Column, ForeignKey
+from sqlalchemy import Column, ForeignKey, Boolean
 from sqlalchemy import Integer, DateTime
 from sqlalchemy.orm import relationship
 
@@ -13,6 +13,8 @@ class Subscription(ModelBase):
     start = Column(DateTime(timezone=True), default=datetime.utcnow, nullable=False)
     end = Column(DateTime(timezone=True), nullable=False)
     type = Column(Integer, nullable=False)
+
+    notification_sended = Column(Boolean, nullable=False, default=False)
 
     place_id = Column(Integer, ForeignKey("places.id"), nullable=False)
     place = relationship("Place", uselist=False, lazy='joined')
